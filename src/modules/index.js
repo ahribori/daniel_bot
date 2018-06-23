a86ed8a1dd13749fb7311085822faef51079e209
ahribori <ahribori@gmail.com>
2018-06-23 12:43:44 +0900

    Production 모드와 아닐때 불러오는 확장자 구분

--- src/modules/index.js
+++ src/modules/index.js
@@ -2,10 +2,20 @@ import fs from 'fs';
 import path from 'path';
 
 const filePathList = fs.readdirSync(__dirname);
-const filter = process.env.NODE_ENV === 'production' ? new RegExp(/(.js)$/) : new RegExp(/(.dev.js)$/);
 filePathList.forEach(file => {
-    if (file !== 'index.js' && filter.test(file)) {
-        console.log(file)
-        require(path.join(__dirname, file));
+    if (file !== 'index.js' && new RegExp(/(.js)$/).test(file)) {
+        if (process.env.NODE_ENV === 'production') {
+            // PRODUCTION
+            if (!new RegExp(/(.dev.js)$/).test(file)) {
+                require(path.join(__dirname, file));
+                console.log(file);
+            }
+        } else {
+            // DEVELOPMENT
+            if (new RegExp(/(.dev.js)$/).test(file)) {
+                require(path.join(__dirname, file));
+                console.log(file);
+            }
+        }
     }
 });
